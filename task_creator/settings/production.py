5d9ad02be56268c8e9636512cb4ef50de32a719b
karim <karimabdelhakim22@hotmail.com>
2019-06-14 00:39:16 +0200

    add from emil in production

--- task_creator/settings/production.py
+++ task_creator/settings/production.py
@@ -2,7 +2,7 @@ import dj_database_url
 from task_creator.utils import get_dot_env
 
 ENV = get_dot_env()
-DEBUG = True
+DEBUG = False
 
 DATABASES = {"default": dj_database_url.config(conn_max_age=500)}
 
@@ -11,4 +11,5 @@ EMAIL_HOST = "in-v3.mailjet.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = ENV.str("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = ENV.str("EMAIL_HOST_PASSWORD")
+FROM_EMAIL = ENV.str("FROM_EMAIL")
 
